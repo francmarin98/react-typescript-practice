c9990380da64469e3685caf609a67d1899440de8
franciscomarin98 <francmarinc@gmail.com>
2021-04-16 11:24:54 -0500

    Tipos basicos listos

--- src/typescript/TiposBasicos.tsx
+++ src/typescript/TiposBasicos.tsx
@@ -1,9 +1,22 @@
 import { Fragment } from 'react'
 
 const TiposBasicos = () => {
+
+    //String
+    let nombre: string | number = 'Francisco';
+    //Number
+    const edad: number = 22;
+    //Boolean
+    const estaActivo:boolean = true;
+    //Arrays
+    const poderes:string[] = ['Velocidad', 'Volar', 'Programar']
+
     return (
         <Fragment>
             <h3>Tipos Básicos</h3>
+            {nombre}, {edad}, {(estaActivo) ? 'Activo' : 'Inactivo'}
+            <hr/>
+            <p>Poderes: {poderes.join(', ')}</p>
         </Fragment>
     )
 }
