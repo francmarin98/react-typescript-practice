16047dd4da224b9b39c4b30ac932347323400c6b
franciscomarin98 <francmarinc@gmail.com>
2021-04-16 11:03:39 -0500

    Configurando el proyecto para trabajar

---
+++ src/typescript/TiposBasicos.tsx
@@ -0,0 +1,11 @@
+import { Fragment } from 'react'
+
+const TiposBasicos = () => {
+    return (
+        <Fragment>
+            <h3>Tipos Básicos</h3>
+        </Fragment>
+    )
+}
+
+export default TiposBasicos
